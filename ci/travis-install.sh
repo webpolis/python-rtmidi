3407ab5acfbd02140d9da1067e3c58b6c51c053b
Christopher Arndt <chris@chrisarndt.de>
2019-01-18 15:54:53 +0100

    Second try at making multi-OS Travis builds work
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -5,24 +5,33 @@
 
 set -ev
 
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    PYTHON=python
+fi
+
+# Report Python version used for this build
+"$PYTHON" --version
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # Install macOS / OS X build dependencies (i.e. Python) via official
     # installers from python.org
     wget -O python.pkg "$PYTHON_INSTALLER_URL"
     test "$(md5 -q python.pkg)" = $PYTHON_INSTALLER_MD5
     sudo installer -pkg python.pkg -target /
+    EXTRA_PIP_ARGS="--user"
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     # Install Linux build dependencies via package manager
-    sudo apt-get -y install build-essential libasound2-dev libjack-jackd2-dev
+    sudo apt-get update -q -y
+    sudo apt-get -y -q install build-essential libasound2-dev libjack-jackd2-dev
+    EXTRA_PIP_ARGS=""
 else
     echo "Unsupported build OS environment $TRAVIS_OS_NAME. Aborting." > /dev/stderr
     exit 1
 fi
 
-$PYTHON --version
 # Update packaging tools
-$PYTHON -m pip --disable-pip-version-check install --user -U pip setuptools wheel
+"$PYTHON" -m pip --disable-pip-version-check install ${EXTRA_PIP_ARGS} -U pip setuptools wheel
 # Install Python build and deployment dependencies
-$PYTHON -m pip install --user -U Cython twine
+"$PYTHON" -m pip install ${EXTRA_PIP_ARGS} -U Cython twine
 # Show installed Python packages
-$PYTHON -m pip freeze --all
+"$PYTHON" -m pip freeze --all
